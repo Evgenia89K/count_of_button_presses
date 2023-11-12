f80df7ddf5e6fbaa47379dd94055e0e3fa046f64
Evgenia89K <106976888+Evgenia89K@users.noreply.github.com>
2023-11-12 16:18:17 +0700

    Add files via upload

---
+++ example/example.ino
@@ -0,0 +1,16 @@
+#include <_BUTTON_.h>
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, INPUT_PULLUP);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  Serial.println(_BUT_(8, 1000, 80, 8));
+  
+  
+
+}
